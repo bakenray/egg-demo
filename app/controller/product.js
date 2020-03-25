dda27b304a16d94e8d6fccd9295adfc4adcc0b98
bakenray <lpqstrong@163.com>
2020-03-25 10:57:34 +0800

    完成基础 添加 获取 获取单个

--- app/controller/product.js
+++ app/controller/product.js
@@ -7,5 +7,18 @@ class ProductController extends Controller{
         const {ctx} = this;
         ctx.body = {products}
     }
+    async getOneById(){
+        const {ctx} = this;
+        const {id} = ctx.query;
+        const product = products.find(p=>p.id === Number(id));
+        ctx.body = {product};
+    }
+    async addOne(){
+        const {ctx} = this;
+        const {product} = ctx.request.body;
+        if(product) products.push(product);
+        ctx.body = {product};
+    }
+
 }
 module.exports = ProductController;
--- app/router.js
+++ app/router.js
@@ -7,4 +7,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/product', controller.product.index);
+  router.get('/product/getOneById', controller.product.getOneById);
+  router.post('/product/addOne', controller.product.addOne);
 };
