ba68b05261c9933578a3de72504366c994f675b7
bakenray <lpqstrong@163.com>
2020-03-26 09:39:34 +0800

    加入loader

--- app/controller/product.js
+++ app/controller/product.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const {Controller} = require('egg');
+const { Controller } = require('egg');
 const ProductModel = require('../model/product');
 const productModel = new ProductModel();
 
-class ProductController extends Controller{
-    async index(){
-        const {ctx} = this;
-        const products = await productModel.list();
-        ctx.body = { products };
-    }
-    async getOneById(){
-        const { ctx } = this;
-        const { id } = ctx.query;
-        const product = await productModel.getOneById(id);
-        ctx.body = {product};
-    }
-    async addOne(){
-        const {ctx} = this;
-        const {product} = ctx.request.body;
-        await productModel.addOne(product);
-        ctx.body = {product};
-    }
+class ProductController extends Controller {
+  async index() {
+    const { ctx } = this;
+    const products = await this.ctx.model.product.list();
+    ctx.body = { products };
+  }
+  async getOneById() {
+    const { ctx } = this;
+    const { id } = ctx.query;
+    const product = await this.ctx.model.product.getOneById(id);
+    ctx.body = { product };
+  }
+  async addOne() {
+    const { ctx } = this;
+    const { product } = ctx.request.body;
+    await this.ctx.model.product.addOne(product);
+    ctx.body = { product };
+  }
 }
 module.exports = ProductController;
